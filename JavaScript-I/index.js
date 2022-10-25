fa1989ca81d728be0cb17719044e814cd1aa032c
Abdullah Bhatti <abdullah.bhatti@meezotech.com>
2022-10-26 01:27:34 +0500

    1 to 5

--- JavaScript-I/index.js
+++ JavaScript-I/index.js
@@ -22,6 +22,8 @@ console.log(`Albert Einstein once said, "A person who never made a mistake never
 //Question-5 ----------------------------------------------------------------
 let famous_person = 'Albert Einstein';
 let message = 'A person who never made a mistake never tried anything new.';
-console.log(`${famous_person} once said, "${message}"`);
+// console.log(`${famous_person} once said, "${message}"`);
+
+//Question-6 ----------------------------------------------------------------
 
 
